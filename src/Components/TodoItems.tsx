ed0aea63f849a708ba745a1e269ce182977b506c
Oleksii Petrychenko <apetr@MacBook-Air.local>
2025-03-05 13:32:55 +0200

    add task solution

---
+++ src/Components/TodoItems.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+
+type ExtendedTodo = Todo & {
+  pendingToggle?: boolean;
+  pendingDelete?: boolean;
+};
+
+type TodoItemProps = {
+  todo: ExtendedTodo;
+  onDelete: (id: Todo['id']) => void;
+  onToggle: (id: number) => void;
+};
+
+export const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  onDelete,
+  onToggle,
+}) => {
+  const { id, title, completed } = todo;
+  const isPending = id < 0 || todo.pendingDelete || todo.pendingToggle;
+
+  return (
+    <div data-cy="Todo" className={`todo ${completed ? 'completed' : ''}`}>
+      <label className="todo__status-label" htmlFor={`todo-checkbox-${id}`}>
+        <input
+          id={`todo-checkbox-${id}`} // Ensures proper association
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={completed}
+          onChange={() => onToggle(id)}
+          disabled={isPending}
+        />
+        Mark as completed
+      </label>
+      <span data-cy="TodoTitle" className="todo__title">
+        {title}
+      </span>
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => onDelete(id)}
+        disabled={isPending}
+      >
+        ×
+      </button>
+      <div
+        data-cy="TodoLoader"
+        className={`modal overlay ${isPending ? 'is-active' : ''}`}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+};
